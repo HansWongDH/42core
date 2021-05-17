4efa273aee3a4ea9063b3bf4b6ade262b7d9985a
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-17 12:06:54 +0800

    strjoin.c

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/17 11:18:28 by wding-ha          #+#    #+#             */
+/*   Updated: 2021/05/17 12:06:39 by wding-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	printf("%d", len);
+	join = (char *)malloc(sizeof(char) * (len + 1));
+	if (join == NULL)
+		return (NULL);
+	while (*s1)
+	{
+		join[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2)
+	{
+		join[i] = *s2;
+		i++;
+		s2++;
+	}
+	join[i] = '\0';
+	return (join);
+}
