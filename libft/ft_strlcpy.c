15e45da6d04ec33d9cebc3f49b02fd19a1d9b83b
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 03:19:52 +0800

    fix

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 11:09:02 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 02:47:37 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 03:19:38 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strlcpy(char *dest, const char *src, size_t n)
 	ret = 0;
 	i = 0;
 	if (!dest || !src)
-		return (NULL);
+		return ;
 	while (src[ret])
 		ret++;
 	if (n != 0)
