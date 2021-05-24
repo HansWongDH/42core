73e4754455f5db06639476972b85d47e698b28a1
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-25 00:03:54 +0800

    mapi

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 14:51:30 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/19 21:05:09 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 00:03:29 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str = malloc(sizeof(char) * ft_strlen(s) + 1);
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (s[i])
 	{
 		str[i] = (*f)(i, s[i]);
