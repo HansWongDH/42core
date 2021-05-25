bcb7e6de71755e58ae9297c1cc6ebb5639a93ad6
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-25 20:58:01 +0800

    fixing memcmp.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 10:07:02 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 17:22:45 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 20:47:39 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	if (s1 == s2 || n == 0)
 		return (0);
 	i = 0;
-	while (i < n && ((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
+	while (i < n)
+	{
+		if (unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
-	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	}
+	return (0);
 }
