0b654eb58f5dec24d6b57427c96bac191b312724
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-25 14:26:07 +0800

    fixing

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 11:09:02 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 03:19:38 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 14:22:10 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int	ft_strlcpy(char *dest, const char *src, size_t n)
 
 	ret = 0;
 	i = 0;
-	if (!dest || !src)
-		return ;
 	while (src[ret])
 		ret++;
-	if (n != 0)
+	if (n != 0 && dest)
 	{
 		while (i < (n - 1) && src[i])
 		{
