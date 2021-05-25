ed69dd0f204a8346478f6f874c708d56ca25cbd2
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 17:25:35 +0800

    fixing

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 17:27:55 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 17:20:41 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 17:25:04 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)dest)[i++] = ((unsigned char *)src)[i++];
-		if ((unsigned char *)src[i] == (unsigned char)c)
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+		if (((unsigned char *)src)[i] == (unsigned char)c)
 			return (dest + i + 1);
 	}
 	return (NULL);
