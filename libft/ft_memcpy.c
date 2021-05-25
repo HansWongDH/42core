d9ca7aa5b4501f5c22e3cc31df3c0aa81125e816
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 18:58:13 +0800

    fixing

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 17:08:50 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 18:49:14 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 18:54:50 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	if (!dest && !src)
 		return (NULL);
-	i = n;
-	while (i > 0)
+	i = 0;
+	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
-		i--;
+		i++;
 	}
 	return (dest);
 }
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 18:44:22 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 17:37:42 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 18:56:55 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!src || !dest)
-		return (NULL);
 	if (src < dest)
 	{
 		i = n;
