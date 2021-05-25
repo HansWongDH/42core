8799f7d8037c25eef135edfcc7391f99de28a770
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 22:55:28 +0800

    fixed substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 11:11:56 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/19 21:06:46 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 22:54:48 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	size;
 
 	if (!s)
 		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+		len = 0;
+	if (len > size)
+		len = size - start;
 	sub = malloc(sizeof(char) * len + 1);
 	if (!sub)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (s[start + i] && i < len)
 	{
-		sub[i] = s[start];
+		sub[i] = s[start + i];
 		i++;
 	}
 	sub[i] = '\0';
