00ebe1ea92c930a93a92fafcd07bb80d2e30c55c
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-25 21:57:00 +0800

    fixing

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wding-ha <wding@student.42kl.edu.my>       +#+  +:+       +#+        */
+/*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 08:52:11 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/17 09:29:22 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 21:56:53 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp( const char *s1, const char *s2, size_t n)
 	if (s1 == s2 || n == 0)
 		return (0);
 	i = 0;
-	while (i < n && *s1 == *s2)
+	while (i < n && *s1 == *s2 && *s1 && *s2)
 	{
 		s1++;
 		s2++;
