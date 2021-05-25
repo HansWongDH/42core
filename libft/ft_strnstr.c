b85f829357d47db484f651a528bfd16076be0d3b
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 22:18:35 +0800

    fixed strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding@student.42kl.edu.my>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 08:00:08 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/17 09:30:22 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 22:17:29 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	size_t	j;
 
+	if (!*s2)
+		return ((char *)s1);
 	i = 0;
 	while (s1[i] && i < n)
 	{
 		j = 0;
-		while (s1[i + j] == s2[j])
+		while (s1[i + j] == s2[j] && i + j < n)
 		{
 			if (s2[j + 1] == '\0')
 				return ((char *)s1 + i);
