2128acdf543c1b1aa34358489444208b3801ad5c
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-05-25 22:03:36 +0800

    fixed strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 08:52:11 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 21:56:53 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/25 22:02:28 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ int	ft_strncmp( const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (s1 == s2 || n == 0)
+	if (n == 0)
 		return (0);
 	i = 0;
-	while (i < n && *s1 == *s2 && *s1 && *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	while (i < n - 1 && s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
