0113a7af5e56cde93f1ed67f5895d7f427901e36
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-26 15:55:41 +0800

    ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,16 +6,16 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 12:32:34 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/25 23:46:44 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/26 15:55:22 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-void	freemalloc(char **arr)
+void    **freemalloc(char **arr)
 {
-	int	i;
+	int    i;
 
 	i = 0;
 	while (arr[i])
@@ -28,73 +28,71 @@ void	freemalloc(char **arr)
 
 int	numstr(char *s, char c)
 {
-	int	count;
 	int	i;
-	
-	i = 0;
-	if (s[i] != c && s[i] != '\0')
-		count = 1;
-	else
-		count = 0;
-	while (s[i])
+
+	i = 1;    
+	while (*s)
 	{
-		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-			count++;
+		if (*s == c)
+			i++;
 		s++;
 	}
-	return (count);
+	return (i);
 }
 
-char	**memalloc(char *s, char c, int sep)
+char	**memalloc(char *s, char c)
 {
 	int		i;
+	int		sep;
 	int		len;
 	char	**arr;
 
+	sep = numstr((char *)s, c);
 	arr = (char **)malloc(sizeof(char) * sep);
 	len = 0;
 	sep = 0;
-	i = 0;
 	while (s[i])
 	{
 		len++;
 		if (s[i + 1] == c || s[i + 1] == '\0')
 		{
-			arr[sep] = ft_calloc(len + 1, 1);	
-			if (arr[sep] == NULL)
-			{
-				freemalloc(arr);
-				return (NULL);
-			}
+			arr[sep] = (char *)malloc(sizeof(char) * len + 1);
 			sep++;
 			len = 0;
 		}
 		i++;
 	}
+	if (arr == NULL)
+	{
+		freemalloc(arr);
+		return (NULL);
+	}
 	return (arr);
 }
 
-char	**ft_split(char const *s, char c)
+char    **ft_split(char const *s, char c)
 {
-	char	**arr;
-	int		sep;
-	int		i;
+    char    **arr;
+    int        sep;
+    int        i;
 
-	if (!s)
-		return (NULL);
-	sep = numstr((char *)s, c);
-	arr = memalloc((char *)s, c, sep);
-	if (!arr)
-		return (NULL);
-	i = 0;
-	sep = 0;
-	while (*s)
-	{
-		if (*s && *s != c)
-			arr[sep][i++] = *s;
-		if (*s == c || *s == '\0')
-			i = 0;
-		s++;
-	}
-	return (arr);
+    arr = memalloc((char *)s, c);
+    i = 0;
+    sep = 0;
+    while (*s)
+    {
+        if (*s && *s != c)
+        {
+            arr[sep][i] = *s;
+            i++;
+        }
+        if (*s == c || *s == '\0')
+        {
+            arr[sep][i] = '\0';
+            i = 0;
+            sep++;
+        }
+        s++;
+    }
+    return (arr);
 }
