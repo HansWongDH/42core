1f9b3fb8df9b3dae22a55937578de935be96f319
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-05-26 16:29:39 +0800

    split la

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,93 +6,77 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 12:32:34 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/05/26 15:55:22 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/05/26 16:29:05 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "libft.h"
-
-void    **freemalloc(char **arr)
-{
-	int    i;
-
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
-}
-
-int	numstr(char *s, char c)
+int	word_count(char const *s, char c)
 {
 	int	i;
+	int	word_count;
 
-	i = 1;    
-	while (*s)
-	{
-		if (*s == c)
-			i++;
-		s++;
-	}
-	return (i);
-}
-
-char	**memalloc(char *s, char c)
-{
-	int		i;
-	int		sep;
-	int		len;
-	char	**arr;
-
-	sep = numstr((char *)s, c);
-	arr = (char **)malloc(sizeof(char) * sep);
-	len = 0;
-	sep = 0;
+	i = 0;
+	if (s[i] != c && s[i] != '\0')
+		word_count = 1;
+	else
+		word_count = 0;
 	while (s[i])
 	{
-		len++;
-		if (s[i + 1] == c || s[i + 1] == '\0')
-		{
-			arr[sep] = (char *)malloc(sizeof(char) * len + 1);
-			sep++;
-			len = 0;
-		}
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			word_count++;
 		i++;
 	}
-	if (arr == NULL)
-	{
-		freemalloc(arr);
-		return (NULL);
-	}
-	return (arr);
+	return (word_count);
 }
 
-char    **ft_split(char const *s, char c)
+int	free_arr(char **arr, int count)
 {
-    char    **arr;
-    int        sep;
-    int        i;
+	while (--count >= 0)
+		free(arr[count]);
+	free(arr);
+	return (0);
+}
 
-    arr = memalloc((char *)s, c);
-    i = 0;
-    sep = 0;
-    while (*s)
-    {
-        if (*s && *s != c)
-        {
-            arr[sep][i] = *s;
-            i++;
-        }
-        if (*s == c || *s == '\0')
-        {
-            arr[sep][i] = '\0';
-            i = 0;
-            sep++;
-        }
-        s++;
-    }
-    return (arr);
+int	split_it(char const *s, char c, char **arr, int words)
+{
+	int		count;
+	int		len;
+	char	*str;
+
+	count = 0;
+	while (count < words)
+	{
+		len = 0;
+		while (s[len] != c && s[len] != '\0')
+			len++;
+		if (len)
+		{
+			str = ft_calloc(len + 1, 1);
+			if (!str)
+				return (free_arr(arr, count));
+			arr[count] = ft_memcpy(str, s, len);
+			count++;
+			s = &s[len];
+		}
+		while (*s == c)
+			s++;
+	}
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	int		words;
+
+	if (!s)
+		return (NULL);
+	words = word_count(s, c);
+	arr = (char **)malloc(words * sizeof(char *) + 1);
+	if (!arr)
+		return (NULL);
+	if (!split_it(s, c, arr, words))
+		return (NULL);
+	arr[words] = NULL;
+	return (arr);
 }
