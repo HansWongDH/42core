615cac846577df4eb0501fa790e0809bcd501c99
Wong Ding hang <wding-ha@u90z06s02.42kl.edu.my>
2021-06-30 21:52:19 +0800

    Fixing

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,50 +6,49 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 08:31:29 by wding-ha          #+#    #+#             */
-/*   Updated: 2021/06/30 14:19:17 by wding-ha         ###   ########.fr       */
+/*   Updated: 2021/06/30 21:51:32 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	freestr(char *str)
+void	freestr(char **str)
 {
-	if (str != NULL)
+	if (str || *str)
 	{
-		free(str);
-		str = NULL;
+		free(*str);
+		*str = NULL;
 	}
 }
 
-int	processline(char *save, char **line)
+int	processline(char **save, char **line)
 {
 	int		len;
 	char	*hold;
 
 	len = 0;
-	while (save[len] && save[len] != '\n')
+	while (*save[len] && *save[len] != '\n')
 		len++;
-	if (save[len] == '\n')
+	if (*save[len] == '\n')
 	{
-		*line = ft_substr(save, 0, len);
-		hold = ft_strdup(save + len);
+		*line = ft_substr(*save, 0, len);
+		hold = ft_strdup(*save + len );
 		freestr(save);
-		save = hold;
-		freestr(hold);
+		*save = hold;
 	}
 	return (1);
 }
 
-int	checkEOF(char *buf, char *save, char **line)
+int	checkEOF(char *buf, char **save, char **line)
 {
 	if (buf[0] == '\0')
 	{
 		*line = malloc(sizeof(char) * 1);
 		*line[0] = '\0';
 	}
-	else if (save)
+	else
 	{
-		*line = ft_strdup(save);
+		*line = ft_strdup(*save);
 		freestr(save);
 	}
 	return (0);
@@ -61,23 +60,28 @@ int	get_next_line(int fd, char **line)
 	char		buf[BUFFER_SIZE + 1];
 	char		*str;
 	static char	*save[400];
+	int			ret;
 
 	if (fd < 0 || !line)
 		return (-1);
-	while (read(fd, buf, BUFFER_SIZE) > 0)
+	
+	ret = read(fd, buf, BUFFER_SIZE);
+	while (ret > 0)
 	{
+		buf[ret] = '\0';
 		if (!save[fd])
 			save[fd] = ft_strdup(buf);
 		else
 		{
 			str = ft_strjoin(save[fd], buf);
-			freestr(save[fd]);
+			freestr(&save[fd]);
 			save[fd] = str;
 		}
 		if ((ft_strchr(save[fd], '\n')))
-			return (processline(save[fd], line));
+			return (processline(&save[fd], line));
+		ret = read(fd, buf, BUFFER_SIZE);
 	}
-	if (read(fd, buf, BUFFER_SIZE) < 0)
+	if (ret < 0)
 		return (-1);
-	return (checkEOF(buf, save[fd], line));
+	return (checkEOF(buf, &save[fd], line));
 }
