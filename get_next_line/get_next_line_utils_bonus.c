5704b0cc298ab364036cb096615821b33eaa0630
hans <hansdhwong@HANS-DESKTOP.localdomain>
2021-07-01 22:35:16 +0800

    Adding bonus

---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,95 @@
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+		len = 0;
+	if (len > size)
+		len = size - start;
+	sub = malloc(sizeof(char) * len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*ret;
+	int		len;
+	int		i;
+
+	len = ft_strlen(str);
+	ret = malloc(sizeof(char) * len + 1);
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ret[i] = str[i];
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		len;
+	int		i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	join = malloc(sizeof(char) * (len + 1));
+	if (!join)
+		return (NULL);
+	while (*s1)
+	{
+		join[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2)
+	{
+		join[i] = *s2;
+		i++;
+		s2++;
+	}
+	join[i] = '\0';
+	return (join);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str && *str != (char)c)
+		str++;
+	if (*str == (char)c)
+		return ((char *)str);
+	return (NULL);
+}
